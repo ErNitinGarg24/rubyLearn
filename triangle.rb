2e623bab5dd45a0617510131e1f0630cecd39edb
Nitin Garg <er.nitingarg2013@gmail.com>
2022-09-20 19:33:27 +0530

    creating tringle in Ascii

---
+++ triangle.rb
@@ -0,0 +1,19 @@
+# =======================================================
+# Making Triangle   
+# =======================================================
+
+puts "Which character you want to use , to make the triangle?"
+character = gets.chomp
+
+puts "How many lines you would like to iterate the triangle?"
+rows = gets.chomp.to_i
+
+character_rep = 1
+width = (character.length * ((rows * 2)-1)) + 20
+
+puts character, character.length , character_rep, width, rows
+
+rows.times do 
+    puts (character * character_rep).center(width)
+    character_rep += 2
+end
