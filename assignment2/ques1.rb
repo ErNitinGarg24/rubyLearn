d807671bb4975e4da688b43a6b41841fabb26db5
Nitin Garg <er.nitingarg2013@gmail.com>
2022-09-21 18:03:06 +0530

    minor updates

---
+++ assignment2/ques1.rb
@@ -0,0 +1,8 @@
+=begin
+============================================================
+Q: 1. Write a program which creates a class “Student” with the following Data Members rollNum, studName, mark1, mark2, mark3, totalMarks Methods setStudDetails() – which sets the values to all the data members except totalMarks. calculateTotal() - which calculate the totalMarks displayStudDetails() – which displays rollNum, studName and totalMarks Create a class StudentDemo to test the functionality of Student class.
+
+Please define the variable names, classes, methods and all coding in ruby way of coding.
+============================================================
+=end
+
