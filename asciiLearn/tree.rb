9a232e3fab8f54046ef3e6b9a6bd41e20f5ed83d
Nitin Garg <er.nitingarg2013@gmail.com>
2022-09-21 13:37:12 +0530

    Adding comments to tree.rb file

--- asciiLearn/tree.rb
+++ asciiLearn/tree.rb
@@ -4,10 +4,9 @@
 
 puts "How much lenght you want for your tree?"
 length = gets.chomp.to_i
-
 tree_slashes = 1
 width = ((length * 2)-1) + 20
-
+# Here we are creating the toop part of the tree
 length.times do |i|
     puts (("/"*tree_slashes)+("\\" *tree_slashes)).center(width)
     if i > 0
@@ -15,4 +14,5 @@ length.times do |i|
     end
     tree_slashes += 1
 end
+# This will work as steam for the tree
 puts "||".center(width)
