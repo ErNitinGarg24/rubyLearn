3dc6b15a99d6d117900118c18e2bfba2e50b354f
Nitin Garg <er.nitingarg2013@gmail.com>
2022-09-22 18:01:08 +0530

    page created for Question 4

---
+++ assignment2/ques4.rb
@@ -0,0 +1,11 @@
+=begin
+============================================================
+Q:4. Write a program to read a file and do below operations
+
+
+
+a) replaces all numbers to words (eg 1 to one)
+
+b) Write the replaced output to a new file
+============================================================
+=end
