16227501b550ddcd11f5e5d70d1721e2810628b7
Nitin Garg <er.nitingarg2013@gmail.com>
2022-09-26 18:17:58 +0530

    Cartesian Product, Commutative Law are done in ques 3

--- assignment2/ques3.rb
+++ assignment2/ques3.rb
@@ -30,13 +30,48 @@ De Morgan's Law :
 # Requiring set to perform actions
 require 'set'
 
-A = Set[1,2,3,4]
+# A = Set[1,2,3,4]
+A = Set[1,3, 5, 7]
 
 B = Set[1,2,3,4] 
 
+# Let U be the universal set
+U = A|B
 # puts A, B
 
 puts "A U B : #{A|B}"
 puts "A N B : #{A&B}"
+puts U
 
-def cartesianProduct()
+
+# A Compliment :
+#   The complement of a set is the set that includes all the elements of the universal set that are not present in the given set
+puts "Compliment of A ie.  A'(Symbolic representstion of Compliment of A) : #{U - A}"
+
+# cartesian Product:
+puts "cartesianProduct of A and B is : #{A.to_a.product(B.to_a).to_set}"
+
+# Commutative Law  :
+def commutativeLaw(a, b)
+    #   This law indicates 
+    #       1) A U B = B U A
+    #           puts "A U B : #{a|b} and B U A : #{b|a}"
+    #           puts "A U B = B U A : #{(a|b = b|a)? "true" : "false"}"
+    #       2) A N B = B N A
+    #           puts "A U B = B U A : #{(a&b = b&a)? "true" : "false"}"
+    puts "A U B = B U A : #{((a|b = b|a) && (a&b = b&a))? "Commutative law proved" : "false"}"
+end
+commutativeLaw(U, A)
+commutativeLaw(U, B)
+commutativeLaw(B, A)
+commutativeLaw(A, B)
+commutativeLaw(B, U)
+commutativeLaw(A, U)
+
+
+# Associative Law :
+def associativeLaw(a, b, c)
+    
+end
+
+# De Morgan's Law :
