e584df28d498975d4397ed7de86544edaed99bc6
Nitin Garg <er.nitingarg2013@gmail.com>
2022-09-30 11:52:52 +0530

    Created BookCricket.rb file

---
+++ assignment3/bookCricket.rb
@@ -0,0 +1,61 @@
+=begin
+============================================================
+Q: Hope everyone played book cricket in school days, i want that game to be created in a more interactive way using ruby
+
+It should ask for Match type odi /test- 2 innings / t20
+
+if odi - 2 overs to be played and test mean 2 overs, 2 innings, 20-20 - 1over
+
+Toss
+
+Playing 11
+
+Choose captain
+
+You can choose who can come next for batting
+
+Have 1,2,3,4,6, Wicket(caught behind, stumping, lbw, runout, catch out, bold), Wide, byes, leg byes etc as random generator
+
+over changes, strike changes applicable
+
+
+
+Score Card has to be written in file and displayed whenever needed or after every over.
+
+Run rates
+
+Required rates
+
+Strike rates
+
+Bowling economy
+
+
+
+Write unit test cases using rspec whereever necessary. Utilize class, modules,methods and all the concepts learnt so far effectively in a ruby way
+
+============================================================
+=end
+
+
+# ===================================================================================
+# Utilize class, modules,methods and all the concepts learnt so far effectively in a ruby way
+# ===================================================================================
+
+# 1) It should ask for Match type odi /test- 2 innings / t20
+#   1.1) if odi - 2 overs to be played and 
+#   1.2) test mean 2 overs, 2 innings, 
+#   1.3) 20-20 - 1over
+# 2) Toss
+# 3) Playing 11
+#   3.1) Choose captain
+#   3.2) You can choose who can come next for batting
+# 4) Have 1,2,3,4,6, Wicket(caught behind, stumping, lbw, runout, catch out, bold), Wide, byes, leg byes etc as random generator
+# 5) over changes, strike changes applicable
+# 6) Score Card has to be written in file and displayed whenever needed or after every over.
+#   6.1) Create a file named scores, and write details on that
+#       6.1.1) Run rates
+#       6.1.2) Required rates
+#       6.1.3) Strike rates
+#       6.1.4) Bowling economy
+# 7) Write unit test cases using rspec whereever necessary.
